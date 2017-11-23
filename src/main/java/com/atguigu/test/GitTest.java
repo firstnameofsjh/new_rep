5cec96510b8666f6b06e32d50f53cab61a9dc38f
shijiahao <shijiahao@atguigu.com>
2017-11-23 14:43:24 +0800

    commit ob spring - tools

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -3,8 +3,8 @@ package com.atguigu.test;
 public class GitTest {
 	
 	public static void main(String[] args) {
-		System.out.println("哈哈发福!");
-		System.out.println("新内容GitHub");
-		System.out.println("新内容GitHub55555555");
+		System.out.println("ddddddsddd");
+		System.out.println("dddddGitHub");
+		System.out.println("ddddddddddd");
 	}
 }
