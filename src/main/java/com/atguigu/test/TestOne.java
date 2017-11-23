e6304733c7178adbdaf39120a34eb0551bdf92b5
shijiahao <shijiahao@atguigu.com>
2017-11-23 18:04:58 +0800

    commit on eclipse

--- src/main/java/com/atguigu/test/TestOne.java
+++ src/main/java/com/atguigu/test/TestOne.java
@@ -6,6 +6,12 @@ public class TestOne {
 	public static void main(String[] args) {
 		System.err.println("adadadadadadadada");
 		System.err.println("adadadadadadadada");
+		System.err.println("adadadadadadadada");
+		System.err.println("adadadadadadadada");
+		System.err.println("adadadadadadadada");
+		System.err.println("adadadadadadadada");
+		System.err.println("adadadadadadadada");
+		System.err.println("adadadadadadadada");
 
 	}
 }
