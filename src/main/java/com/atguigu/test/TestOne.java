e9d4ab4f551425469e1aa8139b0581341fa4c6ec
shijiahao <shijiahao@atguigu.com>
2017-11-23 15:13:48 +0800

    000

--- src/main/java/com/atguigu/test/TestOne.java
+++ src/main/java/com/atguigu/test/TestOne.java
@@ -6,6 +6,6 @@ public class TestOne {
 	public static void main(String[] args) {
 		System.err.println("adadadadadadadada");
 		System.err.println("adadadadadadadada");
-		System.err.println("adadadadadadadada");
+		System.err.println("adadadadadadada00000000000000000000000000000000000000da");
 	}
 }
