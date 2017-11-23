3683aea7651858f9a221a2da86ef221b5031d796
shijiahao <shijiahao@atguigu.com>
2017-11-23 15:29:15 +0800

    commit on eclipse

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -5,10 +5,6 @@ public class GitTest {
 	public static void main(String[] args) {
 			System.out.println("aaaaaaaaaaaa");
 			System.out.println("aaaaaaaaaaaa");
-			System.out.println("aaaaaaaa222222aaaa666");
-			System.out.println("aaaaaaaaaaaa");
-			System.out.println("aaaaaaaaaaaa");
-			
 			System.out.println("aaaaaaaaaaaa");
 	}
 }
